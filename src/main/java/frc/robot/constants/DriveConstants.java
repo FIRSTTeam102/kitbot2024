e6f5709e5fa54114cb531fc32d03a506f2d6bc95
RushaliB <rushalibetala@gmail.com>
2024-01-27 14:16:11 -0500

    Added drive constants

---
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -0,0 +1,10 @@
+package frc.constants;
+
+public class DriveConstants {
+    public static final int flMotorId = 10;
+    public static final int blMotorId = 11;
+    public static final int frMotorId = 12;
+    public static final int brMotorId = 13;
+
+
+}
