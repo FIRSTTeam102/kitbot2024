ec9bdb97ccf5bd3427e72f1f74f5eec211cd0b28
Joseph Murdock <JoeMurdockVirginia@gmail.com>
2024-02-17 11:59:59 -0500

    Added motor IDs

--- src/main/java/frc/robot/constants/ShooterConstants.java
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -1,7 +1,7 @@
 package frc.robot.constants;
 
 public final class ShooterConstants {
-    public static final int ShooterMotorID = 0;
-    public static final int HolderMotorID = 0;
+    public static final int ShooterMotorID = 25;
+    public static final int HolderMotorID = 24;
 
 }
